c7a85722f7abeca4c23ac664d93f89c75886d402
Ilana Lipkina <ilanalipkina@gmail.com>
2024-01-06 11:44:10 +0400

    refactor(extended-1.2):specify alternative content for multimedia

--- app.js
+++ app.js
@@ -48,6 +48,7 @@ var createNewTaskElement=function(taskString){
     deleteButton.className="tasks-list__delete-button";
     deleteButtonImg.src='./remove.svg';
     deleteButtonImg.className = "tasks-list__delete-button-icon";
+    deleteButtonImg.alt = "Delete icon";
     deleteButton.appendChild(deleteButtonImg);
 
 
